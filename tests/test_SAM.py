97d343cf1543fba79d7c776adb4da7a7cae469c1
MarcoLandtHayen <malaha@gmx.net>
2022-06-01 15:13:27 +0200

    Fix and parameterize test_SAM

--- tests/test_SAM.py
+++ tests/test_SAM.py
@@ -1,3 +1,4 @@
+import pytest
 import xarray as xr
 from numpy.testing import assert_almost_equal
 from pathlib import Path
@@ -5,30 +6,32 @@ from climate_index_collection.indices import southern_annular_mode
 from climate_index_collection.data_loading import load_data_set
 
 
-def test_SAM_metadata():
+@pytest.mark.parametrize("source_name", ["FOCI", "CESM"])
+def test_SAM_metadata(source_name):
     """Ensure that index only contains time dimension."""
     # Load FOCI test data
     TEST_DATA_PATH = Path(__file__).parent / "../data/test_data/"
-    foci_ds = load_data_set(data_path=TEST_DATA_PATH, data_source_name="FOCI")
+    data_set = load_data_set(data_path=TEST_DATA_PATH, data_source_name=source_name)
 
     # Calculate SAM index
-    foci_SAM = southern_annular_mode(foci_ds, slp_name="slp")
+    SAM = southern_annular_mode(data_set)
 
     # Check, if calculated SAM index only has one dimension: 'time'
-    assert foci_SAM.dims[0] == 'time'
-    assert len(foci_SAM.dims) == 1
+    assert SAM.dims[0] == 'time'
+    assert len(SAM.dims) == 1
     
-    
-def test_SAM_standardisation():
+
+@pytest.mark.parametrize("source_name", ["FOCI", "CESM"])
+def test_SAM_standardisation(source_name):
     """Ensure that standardisation works correctly."""
     # Load FOCI test data
     TEST_DATA_PATH = Path(__file__).parent / "../data/test_data/"
-    foci_ds = load_data_set(data_path=TEST_DATA_PATH, data_source_name="FOCI")
-    
+    data_set = load_data_set(data_path=TEST_DATA_PATH, data_source_name=source_name)
+
     # Calculate SAM index
-    foci_SAM = southern_annular_mode(foci_ds, slp_name="slp")
+    SAM = southern_annular_mode(data_set)
 
     # Check, if calculated SAM index has zero mean and unit std dev:
-    assert_almost_equal(actual=foci_SAM.mean('time').values[()], desired=0, decimal=3)
-    assert_almost_equal(actual=foci_SAM.std('time').values[()], desired=1, decimal=3)
+    assert_almost_equal(actual=SAM.mean('time').values[()], desired=0, decimal=3)
+    assert_almost_equal(actual=SAM.std('time').values[()], desired=1, decimal=3)
     
