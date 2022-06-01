4122d77cd016eec821a4ae40b53f58302a493f83
Willi Rath <wrath@geomar.de>
2022-06-01 13:19:05 +0200

    Add data loading functions

---
+++ climate_index_collection/data_loading.py
@@ -0,0 +1,63 @@
+import xarray as xr
+from pathlib import Path
+
+
+def find_data_files(data_path="data/test_data/", data_source_name="FOCI"):
+    """Find all files for given data source.
+
+    Parameters
+    ----------
+    data_path: str | path
+        Location of the data files. Defaults to "data/test_data/".
+    data_source_name: str
+        Name of the model dataset. Defaults to "FOCI".
+
+    Returns
+    -------
+    list
+        Paths to all data files.
+
+    """
+    data_files = list(sorted(Path(data_path).glob(f"{data_source_name}/*.nc")))
+    return data_files
+
+
+def load_data_set(data_path="data/test_data/", data_source_name="FOCI", **kwargs):
+    """Load dataset.
+
+    Parameters
+    ----------
+    data_path: str | path
+        Location of the data files. Defaults to "data/test_data/".
+    data_source_name: str
+        Name of the model dataset. Defaults to "FOCI".
+
+    All kwargs will be passed to xarray.open_mfdataset. Use this for, e.g., chunking.
+
+    Returns
+    -------
+    xarray.Dataset
+        Multifile dataset with (pointers to) all data.
+
+    """
+    data_files = find_data_files(data_path=data_path, data_source_name=data_source_name)
+    raw_data_set = xr.open_mfdataset(data_files, **kwargs)
+    data_set = standardize_metadata(raw_data_set)
+
+    return data_set
+
+
+def standardize_metadata(raw_data_set=None):
+    """Standardize metadata.
+
+    Parameters
+    ----------
+    raw_data_set: xarray.Dataset
+        Dataset with potentially non-standard metadata.
+
+    Returns
+    xarray.Dataset
+        Dataset with standardised metadata.
+
+    """
+    return raw_data_set
---
+++ tests/test_data_loading.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+import pytest
+
+from climate_index_collection.data_loading import load_data_set
+
+
+TEST_DATA_PATH = Path(__file__) / "../data/test_data/"
+
+
+@pytest.mark.parametrize("source_name", ["FOCI", "CESM"])
+def test_loading(source_name):
+    data_set = load_data_set(data_path=TEST_DATA_PATH, data_source_name=source_name)
