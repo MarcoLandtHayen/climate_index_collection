2a3420b537aaf1ad9278a6f028537dd8bf88e763
Willi Rath <willirath@users.noreply.github.com>
2022-06-01 12:16:50 +0200

    Add SAM index (#7)
    * Add SAM index PoC
    
    * move sam definition to module
    
    Co-authored-by: MarcoLandtHayen <malaha@gmx.net>

---
+++ climate_index_collection/indices.py
@@ -0,0 +1,27 @@
+def southern_annular_mode(data_set, slp_name="SLP"):
+    """Calculate the southern annular mode index.
+    
+    Parameters
+    ----------
+    data_set: xarray.DataSet
+        Dataset containing a SLP field.
+    slp_name: str
+        Name of the Sea-Level Pressure field. Defaults to "SLP".
+    
+    Returns
+    -------
+    xarray.DataArray
+        Time series containing the SAM index.
+        
+    """
+    slp = data_set[slp_name]
+    
+    slp40S = slp.sel(lat=-40, method="nearest").mean("lon")
+    slp65S = slp.sel(lat=-65, method="nearest").mean("lon")
+    
+    slp_diff = (slp40S - slp65S)
+    
+    SAM_index = (slp_diff - slp_diff.mean("time")) / slp_diff.std("time")
+    SAM_index = SAM_index.rename("SAM")
+    
+    return SAM_index
