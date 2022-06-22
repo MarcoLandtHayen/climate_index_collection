2b42615b3389ec6308e3218e3a5bb6e78bcc6f33
Willi Rath <wrath@geomar.de>
2022-06-22 14:12:45 +0200

    Use data source names

--- tests/test_indices.py
+++ tests/test_indices.py
@@ -5,11 +5,13 @@ import xarray as xr
 
 from numpy.testing import assert_almost_equal
 
-from climate_index_collection.data_loading import load_data_set
-from climate_index_collection.indices import southern_annular_mode
+from climate_index_collection.data_loading import load_data_set, VARNAME_MAPPING
+from climate_index_collection.indices import (
+    southern_annular_mode,
+)
 
 
-@pytest.mark.parametrize("source_name", ["FOCI", "CESM"])
+@pytest.mark.parametrize("source_name", list(VARNAME_MAPPING.keys()))
 def test_SAM_metadata(source_name):
     """Ensure that index only contains time dimension."""
     # Load FOCI test data
@@ -24,7 +26,7 @@ def test_SAM_metadata(source_name):
     assert len(SAM.dims) == 1
 
 
-@pytest.mark.parametrize("source_name", ["FOCI", "CESM"])
+@pytest.mark.parametrize("source_name", list(VARNAME_MAPPING.keys()))
 def test_SAM_standardisation(source_name):
     """Ensure that standardisation works correctly."""
     # Load FOCI test data
@@ -39,7 +41,7 @@ def test_SAM_standardisation(source_name):
     assert_almost_equal(actual=SAM.std("time").values[()], desired=1, decimal=3)
 
 
-@pytest.mark.parametrize("source_name", ["FOCI", "CESM"])
+@pytest.mark.parametrize("source_name", list(VARNAME_MAPPING.keys()))
 def test_SAM_naming(source_name):
     """Ensure that the index is named correctly."""
     # Load FOCI test data
