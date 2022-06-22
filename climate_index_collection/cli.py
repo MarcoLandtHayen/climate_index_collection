0b8dd60271916b6db74674553a4b9b34cd49d690
Willi Rath <wrath@geomar.de>
2022-06-22 13:17:38 +0200

    Move enum and simplify cli

--- climate_index_collection/cli.py
+++ climate_index_collection/cli.py
@@ -2,21 +2,29 @@ import click
 
 
 from climate_index_collection.indices import ClimateIndexFunctions
-
-all_known_indices = tuple(ClimateIndexFunctions.__members__.keys())
+from climate_index_collection.data_loading import VARNAME_MAPPING
 
 
 @click.command()
 @click.option("--input-path", default=".", help="Path to the input data.")
 @click.option("--output-path", default=".", help="Path to the output data.")
-@click.option("--model-name", default="FOCI", help="Name of the input data source.")
 @click.option(
-    "--index-names", multiple=True, default=all_known_indices,
-    help="Space separated list of all indices you want to calculate."
+    "--model-names", default=",".join(tuple(VARNAME_MAPPING.keys())), 
+    help=(
+        f'Space separated list of the input data sources '
+        f'Defaults to: "{",".join(tuple(VARNAME_MAPPING.keys()))}"'
     )
-def run(input_path, output_path, model_name, index_names):
+)
+@click.option(
+    "--index-names", default=",".join(ClimateIndexFunctions.get_all_names()), 
+    help=(
+        f'Space separated list of all indices you want to calculate. '
+        f'Defaults to: "{",".join(ClimateIndexFunctions.get_all_names())}"'
+    )
+)
+def run(input_path, output_path, model_names, index_names):
     """Command line interface to the climate index collection."""
     click.echo(f"Will look for input data in: {input_path}")
     click.echo(f"Will write outputs to: {output_path}")
-    click.echo(f"Will calculate indices for: {model_name}")
-    click.echo(f"Will calculate following indicesr: {index_names}")
+    click.echo(f"Will calculate indices for: {model_names}")
+    click.echo(f"Will calculate following indices: {index_names}")
--- climate_index_collection/indices.py
+++ climate_index_collection/indices.py
@@ -1,20 +1,7 @@
+from ipaddress import collapse_addresses
 from .reductions import mean_unweighted
 from enum import Enum
 from functools import partial
-
-
-class ClimateIndexFunctions(Enum):
-    """Enumeration of all index functions.
-    
-    We can use this to, e.g., iterate over all defined indices.
-
-    >>> for index_func in ClimateIndexFunctions:
-    ...     print(index_func.name) 
-    southern_annular_mode
-    ...    
-
-    """
-    southern_annular_mode = partial(southern_annular_mode)
 
 
 def southern_annular_mode(data_set, slp_name="sea-level-pressure"):
@@ -44,3 +31,22 @@ def southern_annular_mode(data_set, slp_name="sea-level-pressure"):
     SAM_index = SAM_index.rename("SAM")
 
     return SAM_index
+
+
+class ClimateIndexFunctions(Enum):
+    """Enumeration of all index functions.
+    
+    We can use this to, e.g., iterate over all defined indices.
+
+    >>> for index_func in ClimateIndexFunctions:
+    ...     print(index_func.name) 
+    southern_annular_mode
+    ...    
+
+    """
+    southern_annular_mode = partial(southern_annular_mode)
+
+    @classmethod
+    def get_all_names(cls):
+        """Return tuple with all known index names."""
+        return tuple(cls.__members__.keys())
