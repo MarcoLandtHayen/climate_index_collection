88f3d351ee977a1e886d7396197228b9439f1474
MarcoLandtHayen <malaha@gmx.net>
2022-07-01 13:05:28 +0200

    Add NAO index definition station-based

--- climate_index_collection/indices.py
+++ climate_index_collection/indices.py
@@ -33,6 +33,43 @@ def southern_annular_mode(data_set, slp_name="sea-level-pressure"):
     return SAM_index
 
 
+def north_atlantic_oscillation(data_set, slp_name="sea-level-pressure"):
+    """Calculate the North Atlantic Oscillation (NAO) index station-based using grid points closest to
+    Reykjavik (64°9'N, 21°56'W) and Ponta Delgada (37°45'N, 25°40'W).
+
+    Parameters
+    ----------
+    data_set: xarray.DataSet
+        Dataset containing an SLP field.
+    slp_name: str
+        Name of the Sea-Level Pressure field. Defaults to "sea-level-pressure".
+
+    Returns
+    -------
+    xarray.DataArray
+        Time series containing the NAO index.
+
+    """
+    slp = data_set[slp_name]
+
+    slp_northern_station = slp.sel(lat=64, lon=338, method="nearest")
+    slp_southern_station = slp.sel(lat=38, lon=334, method="nearest")
+
+    slp_northern_station_norm = (
+        slp_northern_station - slp_northern_station.mean("time")
+    ) / slp_northern_station.std("time")
+    slp_southern_station_norm = (
+        slp_southern_station - slp_southern_station.mean("time")
+    ) / slp_southern_station.std("time")
+
+    slp_diff = slp_northern_station - slp_southern_station
+
+    NAO_index = slp_northern_station_norm - slp_southern_station_norm
+    NAO_index = NAO_index.rename("NAO")
+
+    return NAO_index
+
+
 class ClimateIndexFunctions(Enum):
     """Enumeration of all index functions.
 
