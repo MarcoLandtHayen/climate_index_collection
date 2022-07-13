259682457794e486fdee5bf4180950739f38e20f
MarcoLandtHayen <malaha@gmx.net>
2022-07-13 08:05:37 +0200

    Add test for creating csv output

--- tests/test_output.py
+++ tests/test_output.py
@@ -1,3 +1,5 @@
+import os
+
 from pathlib import Path
 
 import numpy as np
@@ -10,7 +12,12 @@ from climate_index_collection.indices import (
     north_atlantic_oscillation,
     southern_annular_mode,
 )
-from climate_index_collection.output import compute_index, index_dataarray_to_dataframe, concat_indices
+from climate_index_collection.output import (
+    compute_index,
+    concat_indices,
+    index_dataarray_to_dataframe,
+    run_full_workflow_csv,
+)
 
 
 # Dummy example data
@@ -73,7 +80,7 @@ def test_conversion_to_dataframe(example_data_array):
         == np.array([0.5, 1, -0.3, -0.7])
     )
 
-    
+
 @pytest.mark.parametrize("source_name", list(VARNAME_MAPPING.keys()))
 def test_returntype_of_conversion_to_dataframe(source_name):
     """Ensure that function index_dataarray_to_dataframe returns pandas dataframe."""
@@ -86,30 +93,52 @@ def test_returntype_of_conversion_to_dataframe(source_name):
         data_source_name=source_name,
         index_function=southern_annular_mode,
     )
-    
+
     # Convert to pandas DataFrame
     SAM_df = index_dataarray_to_dataframe(index_data_array=SAM_data_array)
 
     # Check, if calculated SAM index has type pandas.DataFrame
     assert type(SAM_df) is pd.DataFrame
-    
-    
-@pytest.mark.parametrize("source_name", list(VARNAME_MAPPING.keys()))
-def test_concat_indices(source_name):
+
+
+def test_concat_indices():
     """Ensure that resulting dataframe has unique index and does not contain duplicate rows."""
     # Path to test data
     TEST_DATA_PATH = Path(__file__).parent / "../data/test_data/"
 
-    # Compute indices from sources and concatenate resulting dataframes   
+    # Compute indices from sources and concatenate resulting dataframes
     df = concat_indices(
         data_path=TEST_DATA_PATH,
-        data_source_names=['FOCI','CESM'],
-        index_functions=[southern_annular_mode,north_atlantic_oscillation],
-    )    
-    
+        data_source_names=["FOCI", "CESM"],
+        index_functions=[southern_annular_mode, north_atlantic_oscillation],
+    )
+
     # Check, if resulting dataframe has unique index
     assert np.max(df.index.value_counts()) == 1
-    
+
     # Check, if resulting dataframe has NO duplicate rows
-    assert not(any(df.duplicated()))
-    
+    assert not (any(df.duplicated()))
+
+
+def test_run_full_workflow_csv():
+    """Ensure that output is written as csv file."""
+    # Path to test data
+    TEST_DATA_PATH = Path(__file__).parent / "../data/test_data/"
+
+    # Check, if desired output file already exists. If yes, remove it.
+    if os.path.exists("../tests/climate_indices.csv"):
+        os.remove("../tests/climate_indices.csv")
+
+    # Compute indices from sources, concatenate resulting dataframes and output csv file
+    run_full_workflow_csv(
+        data_path=TEST_DATA_PATH,
+        file_name="../tests/climate_indices.csv",
+        data_source_names=["FOCI", "CESM"],
+        index_functions=[southern_annular_mode, north_atlantic_oscillation],
+    )
+
+    # Check, if csv output file exists
+    assert os.path.exists("../tests/climate_indices.csv")
+
+    # Remove test output
+    os.remove("../tests/climate_indices.csv")
