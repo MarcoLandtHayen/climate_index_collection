bb6071b55da1c9e6a60d810a744167e3c512efa7
MarcoLandtHayen <malaha@gmx.net>
2022-08-05 15:04:43 +0200

    Add test data for sea air temperature, sea surface salinity and precipitation plus modify script to extract test data accordingly

--- data/test_data/extract_test_data.sh
+++ data/test_data/extract_test_data.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 
 source_dir="/gxfs_work1/geomar/smomw235/data_user/data_mlandt/"
 
@@ -18,6 +17,16 @@ ncks -O -d time,1,120,1 \
 ncks -O -d time,1,120,1 \
 	/gxfs_work1/geomar/smomw235/data_user/data_mlandt/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0999.Z500.nc \
 	CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0010.Z500.nc
+ncks -O -d time,1,120,1 \
+        /gxfs_work1/geomar/smomw235/data_user/data_mlandt/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0999.TS.nc \
+        CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0010.TS.nc
+ncks -O -d time,1,60,1 \
+        /gxfs_work1/geomar/smomw235/data_user/data_mlandt/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.pop.h.0001-0999.SSS.nc \
+        CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.pop.h.0001-0005.SSS.nc
+ncks -O -d time,1,120,1 \
+        /gxfs_work1/geomar/smomw235/data_user/data_mlandt/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0999.PRECT.nc \
+        CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0010.PRECT.nc
+
 
 # extract 120 time steps of FOCI data
 
@@ -30,3 +39,13 @@ ncks -O -d time,1,120,1 \
 ncks -O -d time,1,120,1 \
 	/gxfs_work1/geomar/smomw235/data_user/data_mlandt/FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-3349_tsw_monthly_1.nc \
 	FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-2359_tsw_monthly_1.nc
+ncks -O -d time,1,120,1 \
+        /gxfs_work1/geomar/smomw235/data_user/data_mlandt/FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-3349_temp2_monthly_1.nc \
+        FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-2359_temp2_monthly_1.nc
+ncks -O -d time_counter,1,60,1 -v sosaline \
+        /gxfs_work1/geomar/smomw235/data_user/data_mlandt/FOCI/FOCI1.3-SW038_1m_23500101_33491231_grid_T.nc \
+        FOCI/FOCI1.3-SW038_1m_23500101_23541231_grid_T.nc
+ncks -O -d time,1,120,1 \
+        /gxfs_work1/geomar/smomw235/data_user/data_mlandt/FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-3349_precip_monthly_1.nc \
+        FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-2359_precip_monthly_1.nc
+
