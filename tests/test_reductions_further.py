ff16e4c62c49ac656f9d3f124bad200f544f9c3a
nilsnevertree <nils.niebaum@gmx.de>
2022-08-07 20:20:37 +0200

    correct testing for monthly anomalies #53 #48

--- tests/test_reductions_further.py
+++ tests/test_reductions_further.py
@@ -1,10 +1,12 @@
 import weakref
-from numpy.testing import assert_almost_equal, assert_allclose
+from numpy.testing import assert_almost_equal
 import numpy as np
 import pytest
 import xarray as xr
 from xarray import DataArray
 import pandas as pd
+import math
+from pathlib import Path
 
 from climate_index_collection.reductions import (
     grouped_mean_weighted,
@@ -13,10 +15,7 @@ from climate_index_collection.reductions import (
     monthly_anomalies_unweighted,
     monthly_anomalies_weighted,
 )
-from climate_index_collection.data_loading import load_data_set
-
-CESM_DATA = load_data_set(data_path="../data/test_data/", data_source_name="CESM")
-FOCI_DATA = load_data_set(data_path="../data/test_data/", data_source_name="FOCI")
+from climate_index_collection.data_loading import load_data_set, VARNAME_MAPPING
 
 # ========
 # CREATE TEST DATA PARAMETERS AND FUNCTIONS
@@ -58,8 +57,9 @@ def create_mean_array(mean, group_unique, group_name) :
                  coords={group_name : group_unique, "lat": lat, 'lon': lon})
     return weights 
 
-# ================
+# ----------
 # First test DataArray
+# ----------
 weights_1 = [1, 2, 3, 2]
 dim_1 = ['a','b','a','c']
 dim_name_1 = group_name_1 = "group" 
@@ -90,15 +90,19 @@ weighted_mean_1 = np.array(
         [1.  , 0.  , 0.  ],
         [1.  , 1.  , 1.  ]]])
 
-weighted_mean_should_1 = create_mean_array(mean = weighted_mean_1,
+weighted_mean_1 = create_mean_array(mean = weighted_mean_1,
                                         group_unique = group_unique_1,
                                         group_name = group_name_1)
 
-# ================
+# ----------
 # Second test DataArray
+# ----------
 dim_2 = pd.to_datetime(["2020-02-13", "2021-06-13", "2021-08-13", "2022-02-13"])
 dim_name_2 = "time"
 weights_2 = dim_2.days_in_month
+weights_2 = create_weight_array(wei = weights_2, 
+                                  dim = dim_2, 
+                                  dim_name=dim_name_2)
 group_unique_2 = np.unique(dim_2.month)
 group_name_2 = "month"
 values_2 = np.array(
@@ -122,6 +126,8 @@ values_2 = np.array(
 data_2 = create_data_array(values = values_2, 
                            dim = dim_2, 
                            dim_name=dim_name_2)
+# also create a dataset
+dataset = data_2.to_dataset(dim=None, name="test", promote_attrs=False)
 
 weighted_mean_2 = np.array(
       [[[57.,  0., 29.],
@@ -135,7 +141,9 @@ weighted_mean_2 = np.array(
        [[ 0., 57.,  0.],
         [57.,  0.,  0.],
         [ 0., 57., 57.]]])
-
+weighted_mean_2 = create_mean_array(mean = weighted_mean_2,
+                                        group_unique=group_unique_2,
+                                        group_name = group_name_2)
 unweighted_mean_2 = np.array(
       [[[57,  0., 28.5,],
         [57, 28.5, 28.5,],
@@ -148,158 +156,240 @@ unweighted_mean_2 = np.array(
        [[ 0., 57.,  0.],
         [57.,  0.,  0.],
         [ 0., 57., 57.]]])
-
-weighted_mean_should_2 = create_mean_array(mean = weighted_mean_2,
-                                        group_unique=group_unique_2,
-                                        group_name = group_name_2)
-unweighted_mean_should_2 = create_mean_array(mean = unweighted_mean_2,
+unweighted_mean_2 = create_mean_array(mean = unweighted_mean_2,
                                         group_unique=group_unique_2,
                                         group_name = group_name_2)
 
-weights_2 =  create_weight_array(wei = weights_2, 
-                                  dim = dim_2, 
-                                  dim_name=dim_name_2)
-# aslo create a dataset
-dataset_2 = data_2.to_dataset(dim=None, name="test", promote_attrs=False)
 
-# calculate the monthly anomalies for the data now nans will be included!
-anomalies_weighted_2 = np.array([
-    values_2[0] - weighted_mean_should_2[0],
-    values_2[1] - weighted_mean_should_2[1],
-    values_2[2] - weighted_mean_should_2[2],
-    values_2[3] - weighted_mean_should_2[0],
-    ])
-anomalies_weighted_2 = create_data_array(values = anomalies_weighted_2, 
-                                  dim = dim_2, 
-                                  dim_name=dim_name_2)
+# monthly anomalies for the data. 
+# nans will be included!
+weighted_anomalies_2 = np.    array(
+        [[[np.nan,   0.,  28.],
+            [  0.,  28.,  28.],
+            [-28., -28.,   0.]],
 
-anomalies_unweighted_2 = np.array([
-    values_2[0] - unweighted_mean_should_2[0],
-    values_2[1] - unweighted_mean_should_2[1],
-    values_2[2] - unweighted_mean_should_2[2],
-    values_2[3] - unweighted_mean_should_2[0],
-    ])
-anomalies_unweighted_2 = create_data_array(values = anomalies_unweighted_2, 
+           [[  0.,   0.,   0.],
+            [  0.,   0.,   0.],
+            [  0.,   0.,   0.]],
+
+           [[  0.,   0.,   0.],
+            [  0.,   0.,   0.],
+            [  0.,   0.,   0.]],
+
+           [[  0.,   0., -29.],
+            [  0., -29., -29.],
+            [ 29.,  29.,   0.]]])
+weighted_anomalies_2 = create_data_array(values = weighted_anomalies_2, 
                                   dim = dim_2, 
                                   dim_name=dim_name_2)
 
-# ========================
-# PYTEST Functions
+unweighted_anomalies_2 = np.array(
+     [[[np.nan,   0. ,  28.5],
+        [  0. ,  28.5,  28.5],
+        [-28.5, -28.5,   0. ]],
+
+       [[  0. ,   0. ,   0. ],
+        [  0. ,   0. ,   0. ],
+        [  0. ,   0. ,   0. ]],
+
+       [[  0. ,   0. ,   0. ],
+        [  0. ,   0. ,   0. ],
+        [  0. ,   0. ,   0. ]],
+
+       [[  0. ,   0. , -28.5],
+        [  0. , -28.5, -28.5],
+        [ 28.5,  28.5,   0. ]]])
+unweighted_anomalies_2 = create_data_array(values = unweighted_anomalies_2, 
+                                  dim = dim_2, 
+                                  dim_name=dim_name_2)
+
+# ========
+# Basic tests with the test data created above
+# ========
 
 @pytest.mark.parametrize("data, weights, dim     , groupby_dim, should",[ 
-    (data_1, weights_1, group_name_1 , group_name_1 , weighted_mean_should_1),
-    (data_2, weights_2, "time" , "time.month" , weighted_mean_should_2),
+    (data_1, weights_1, group_name_1 , group_name_1 , weighted_mean_1),
+    (data_2, weights_2, "time" , "time.month" , weighted_mean_2),
                          ])
 def test_grouped_mean_weighted(data, weights, dim, groupby_dim, should):
     """Checks if the groupby weighting function gives proper results."""
     result = grouped_mean_weighted(dobj=data, weights= weights, dim = dim, groupby_dim= groupby_dim)
     assert result.equals(should)
 
-@pytest.mark.parametrize("data_set, name, should",[ 
-        (dataset_2, "test", weighted_mean_should_2),
-])
-def test_monthly_mean_weighted(data_set, name, should):
+@pytest.mark.parametrize("data_array, should",[(data_2, weighted_mean_2)])
+def test_monthly_mean_weighted(data_array, should):
     """Checks if the monthly mean weighted function gives proper results."""
-    result = monthly_mean_weighted(dobj=data_set)
-    assert result[name].equals(should)
+    result = monthly_mean_weighted(dobj=data_array)
+    assert result.equals(should)
 
-@pytest.mark.parametrize("data_set, name, should",[ 
-        (dataset_2, "test", unweighted_mean_should_2),
-])
-def test_monthly_mean_unweighted(data_set, name, should):
+@pytest.mark.parametrize("data_array, should",[(data_2, unweighted_mean_2)])
+def test_monthly_mean_unweighted(data_array, should):
     """Checks if the monthly mean unweighted function gives proper results."""
-    result = monthly_mean_unweighted(dobj=data_set)
-    assert result[name].equals(should)
+    result = monthly_mean_unweighted(dobj=data_array)
+    assert result.equals(should)
 
-# ------------
-# Monthly anomalies
+# Tests for anomaly functions
 # check if the results are correct
-@pytest.mark.parametrize("data_set, name, should",[ 
-        (dataset_2, "test", anomalies_weighted_2),
-])
-def test_monthly_anomalies_weighted(data_set, name, should):
+@pytest.mark.parametrize("data_array, should",[(data_2, weighted_anomalies_2)])
+def test_monthly_anomalies_weighted(data_array, should):
     """Checks if the monthly anomalies weighted function gives proper results."""
-    result = monthly_anomalies_weighted(dobj=data_set)
-    assert result[name].equals(should)
+    result = monthly_anomalies_weighted(dobj=data_array)
+    assert result.equals(should)
 
-@pytest.mark.parametrize("data_set, name, should",[ 
-        (dataset_2, "test", anomalies_unweighted_2),
-])
-def test_monthly_anomalies_unweighted(data_set, name, should):
+@pytest.mark.parametrize("data_array, should",[(data_2, unweighted_anomalies_2)])
+def test_monthly_anomalies_unweighted(data_array, should):
     """Checks if the monthly anomalies unweighted function gives proper results."""
-    result = monthly_anomalies_unweighted(dobj=data_set)
-    assert result[name].equals(should)
+    result = monthly_anomalies_unweighted(dobj=data_array)
+    assert result.equals(should)
 
-# check if the the temporal means are all close to 0
-@pytest.mark.parametrize("data_set",[ 
-        (dataset_2["test"]),
-        (FOCI_DATA["sea-surface-temperature"]),
-        (FOCI_DATA["sea-level-pressure"]),
-        (FOCI_DATA["geopotential-height"]),
-        (CESM_DATA["sea-surface-temperature"]),
-        (CESM_DATA["sea-level-pressure"]),
-        (CESM_DATA["geopotential-height"]),
-])
-def test_ifmeanis0_monthly_anomalies_unweighted(data_set, rtol = 1e-6):
-    """Checks if the mean of the anomalies are all close to 0 using numpy.allclose()
-    rtol : float 
-        relative accuracy Default of 1e-6.
-    
-    Absolute accuracy is calculated with (max(data_set) - min(data_set)) * rtol
-    For further information look at numpy.allclose()
-    From numpy:
-    "The tolerance values are positive, typically very small numbers. 
-    The relative difference (rtol * abs(b)) and the absolute difference atol are added together 
-    to compare against the absolute difference between a and b."
-    """
-    result = monthly_anomalies_unweighted(dobj=data_set)
-    # to calculate the absolute tolerance, we use :
-    # ( max(data_set) - min(data_set) ) * rtol
-    min_value = data_set.min().values
-    max_value = data_set.max().values
-    atol = (max_value-min_value)*rtol
-    result_mean = result.mean("time")
-    # note that rtol has have no effect on the final values, because desired is 0
-    assert_allclose(
-                actual = result_mean,
-                desired = result_mean*0,
-                rtol = rtol,
-                atol = atol,
-                equal_nan = True)
-# # Check if the mean of the anomalies is 0
+# ========
+# Test if the means created by the anomalies functions are close to zero.
+# Note: The weighted function will only be zero if one creates the monthly mean values
+# ========
 
-@pytest.mark.parametrize("data_set",[ 
-        #(dataset_2["test"]),   This does not work properly !!!
-        (FOCI_DATA["sea-surface-temperature"]),
-        (FOCI_DATA["sea-level-pressure"]),
-        (FOCI_DATA["geopotential-height"]),
-        (CESM_DATA["sea-surface-temperature"]),
-        (CESM_DATA["sea-level-pressure"]),
-        (CESM_DATA["geopotential-height"]),
-])
-def test_ifmeanis0_monthly_anomalies_weighted(data_set, rtol = 1e-3):
-    """Checks if the mean of the anomalies are all close to 0 using numpy.allclose()
-    rtol : float 
-        relative accuracy Default of 1e-6.
-    
-    Absolute accuracy is calculated with (max(data_set) - min(data_set)) * rtol
-    For further information look at numpy.allclose()
-    From numpy:
-    "The tolerance values are positive, typically very small numbers. 
-    The relative difference (rtol * abs(b)) and the absolute difference atol are added together 
-    to compare against the absolute difference between a and b."
+def variable_close_to_desired(original, actual, desired = 0, relative_tolerance = 1e-5):
     """
-    result = monthly_anomalies_weighted(dobj=data_set)
-    # to calculate the absolute tolerance, we use :
-    # ( max(data_set) - min(data_set) ) * rtol
-    min_value = data_set.min().values
-    max_value = data_set.max().values
-    atol = (max_value-min_value)*rtol
-    result_mean = result.mean("time")
-    # note that rtol has have no effect on the final values, because desired is 0
-    assert_allclose(
-                actual = result_mean,
-                desired = result_mean*0,
-                rtol = rtol,
-                atol = atol,
-                equal_nan = True)
+    Checks if all spatial point from the input array "actual" are close to the desired value.
+    This assert is based on the numpy.testing.assert_almost_equal.
+    The decimal used for this is obtained based on the relative tolerance given.
+    It is calculated as described below
+        1. absolute accuracy = (maximum - minimum) * relative tolerance.
+        2. decimal = -1 * Order(absolute accuracy)
+    Maximum and minimum are derived from the "original" DataArray 
+    which was used for the calculation of the DataArray "actual".
+    For some purpose it might make sense to hand original and actual the same DataArray.
+    
+    Parameters
+    ----------
+    original: xarray.DataArray
+        DataArray containing field from which e.g. the anomalies were calulated.
+    actual: xarray.DataArray
+        Dataset with same variables as orignial but containing the values, 
+        which shall be close to the desired value.
+        For instance the monthly anomalies derived from original
+    desired: float
+        Desired value.
+        Default to 0.
+    relative_tolerance : float
+        Relative tolerance which shall be used to derive the decimal accuracy.
+        Default to 1e-5.
+    
+    """
+    min_value = original.min().values
+    max_value = original.max().values
+    absolute_tolerance = (max_value-min_value)*relative_tolerance
+    # calculate the desired decimal accuracy as 
+    # -1 * Order(absolute accuracy)
+    decimal = -1 * math.floor(math.log(absolute_tolerance, 10))
+    assert_almost_equal(
+            actual = actual.values,
+            desired = desired,
+            decimal=decimal)
+
+def all_variables_close_to_desired(original, actual, desired = 0, relative_tolerance = 1e-5):
+    """
+    Checks if all variable from the input DataSet "actual" are close to the desired value.
+    This assert is based on the numpy.testing.assert_almost_equal.
+    The decimal used for this is obtained based on the relative tolerance given.
+    It is calculated as described below
+        1. absolute accuracy = (maximum - minimum) * relative tolerance.
+        2. decimal = -1 * Order(absolute accuracy)
+    Maximum and minimum are derived from the "original" DataSet for each variable independently. 
+    The "original" should be the DataSet from which "actual" was calculated.
+    For some purpose it might make sense to hand original and actual the same DataArray.
+
+    
+    Parameters
+    ----------
+    original: xarray.DataSet
+        Dataset containing field from which e.g. the anomalies were calulated.
+    actual: xarray.DataSet
+        Dataset with same variables as orignial but containing the values, 
+        which shall be close to the desired value.
+        For instance the monthly anomalies derived from original
+    desired: float
+        Desired value.
+        Default to 0.
+    relative_tolerance : float
+        Relative tolerance which shall be used to derive the decimal accuracy 
+        for each variable individually.
+        Default to 1e-5.
+    
+    """
+    for variable in original.keys() :
+        if "time" in variable:
+            continue
+        variable_close_to_desired(original[variable], actual[variable], desired = 0, relative_tolerance = 1e-5)
+
+@pytest.mark.parametrize("source_name", list(VARNAME_MAPPING.keys())+[dataset])
+def test_monthly_anomalies_weighted_zeromean(source_name, relative_tolerance = 1e-5):
+    """Checks if the mean of the monthly anomalies weighted are all close to 0 using
+    all_variables_close_to_desired which is based on numpy.testing.assert_almost_equal.
+    The test will be performed for each spatial gridpoint individually.
+    
+    NOTE: 
+        As this is a weighted mean, the values of the anomalies will not sum up to zero with a convenient .mean("time").
+        One should better check if the monthly_mean_weighted of the anomalies sums up to zero for each month.
+    
+    Parameters
+    ----------
+    source_name: str
+        Test dataset name.
+    relative_tolerance : float
+        Relative tolerance which shall be used to derive the decimal accuracy 
+        for each variable individually.
+        Default to 1e-5.
+
+    """
+    if isinstance(source_name, xr.Dataset) :
+        data_set = source_name
+    else : 
+        # Load test data
+        TEST_DATA_PATH = Path(__file__).parent / "../data/test_data/"
+        data_set = load_data_set(data_path=TEST_DATA_PATH, data_source_name=source_name)
+    
+    anomalies = monthly_anomalies_weighted(dobj=data_set)
+    anomalies_mean = monthly_mean_weighted(anomalies)
+    
+    # check if the mean of the anomalies is close to 0 with desired decimal accuracy 
+    # derived based of relative tolerance for each variable indepenently 
+    all_variables_close_to_desired(
+        original = data_set, 
+        actual= anomalies_mean, 
+        desired = 0, 
+        relative_tolerance = relative_tolerance)
+
+@pytest.mark.parametrize("source_name", list(VARNAME_MAPPING.keys())+[dataset])
+def test_monthly_anomalies_unweighted_zeromean(source_name, relative_tolerance = 1e-5):
+    """Checks if the mean of the monthly anomalies unweighted are all close to 0 using
+    all_variables_close_to_desired which is based on numpy.testing.assert_almost_equal
+    The test will be performed for each spatial gridpoint individually.
+    
+    Parameters
+    ----------
+    source_name: str or DataSet
+        Test dataset name.
+        Or DataSet directly
+    relative_tolerance : float
+        Relative tolerance which shall be used to derive the decimal accuracy 
+        for each variable individually.
+        Default to 1e-5.
+
+    """
+
+    if isinstance(source_name, xr.Dataset) :
+        data_set = source_name
+    else : 
+        # Load test data
+        TEST_DATA_PATH = Path(__file__).parent / "../data/test_data/"
+        data_set = load_data_set(data_path=TEST_DATA_PATH, data_source_name=source_name)
+    
+    anomalies = monthly_anomalies_unweighted(dobj=data_set)
+    anomalies_mean = anomalies.mean("time")
+    # check if the mean of the anomalies is close to 0 with desired decimal accuracy 
+    # derived based of relative tolerance for each variable indepenently 
+    all_variables_close_to_desired(
+        original = data_set, 
+        actual= anomalies_mean, 
+        desired = 0, 
+        relative_tolerance = relative_tolerance)
