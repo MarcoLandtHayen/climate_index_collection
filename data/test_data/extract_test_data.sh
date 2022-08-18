91e9cf1286960ba3066288d695be33c6d2f17d9a
MarcoLandtHayen <95050365+MarcoLandtHayen@users.noreply.github.com>
2022-08-18 11:38:04 +0200

    Update test data (#86)
    * Update test data to have unique time axis (midmonth) and time span (10 yrs)
    
    * Modify data_loading: Do not need to fix years for CESM data any more
    
    * Remove old SSS test data covering only 5 yrs

--- data/test_data/extract_test_data.sh
+++ data/test_data/extract_test_data.sh
@@ -9,20 +9,20 @@ module load nco
 
 # extract 120 time steps of CESM data
 ncks -O -d time,1,120,1 \
-	/gxfs_work1/geomar/smomw235/data_user/data_mlandt/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0999.PSL.nc \
+	/gxfs_work1/geomar/smomw235/data_user/data_mlandt/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0999.PSL.midmonth.nc \
 	CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0010.PSL.nc
 ncks -O -d time,1,120,1 \
-	/gxfs_work1/geomar/smomw235/data_user/data_mlandt/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0999.SST.nc \
+	/gxfs_work1/geomar/smomw235/data_user/data_mlandt/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0999.SST.midmonth.nc \
 	CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0010.SST.nc
 ncks -O -d time,1,120,1 \
-	/gxfs_work1/geomar/smomw235/data_user/data_mlandt/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0999.Z500.nc \
+	/gxfs_work1/geomar/smomw235/data_user/data_mlandt/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0999.Z500.midmonth.nc \
 	CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0010.Z500.nc
 ncks -O -d time,1,120,1 \
         /gxfs_work1/geomar/smomw235/data_user/data_mlandt/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0999.TS.nc \
         CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0010.TS.nc
-ncks -O -d time,1,60,1 -v SALT \
+ncks -O -d time,1,120,1 -v SALT \
         /gxfs_work1/geomar/smomw235/data_user/data_mlandt/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.pop.h.0001-0999.SSS.nc \
-        CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.pop.h.0001-0005.SSS.nc
+        CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.pop.h.0001-0010.SSS.nc
 ncks -O -d time,1,120,1 \
         /gxfs_work1/geomar/smomw235/data_user/data_mlandt/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0999.PRECT.nc \
         CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0010.PRECT.nc
@@ -31,20 +31,20 @@ ncks -O -d time,1,120,1 \
 # extract 120 time steps of FOCI data
 
 ncks -O -d time,1,120,1 \
-	/gxfs_work1/geomar/smomw235/data_user/data_mlandt/FOCI/FOCI1.3-SW038_echam6_ATM_mm_2350-3349_geopoth_pl_monthly_50000.nc \
+	/gxfs_work1/geomar/smomw235/data_user/data_mlandt/FOCI/FOCI1.3-SW038_echam6_ATM_mm_2350-3349_geopoth_pl_monthly_50000_midmonth.nc \
 	FOCI/FOCI1.3-SW038_echam6_ATM_mm_2350-2359_geopoth_pl_monthly_50000.nc
 ncks -O -d time,1,120,1 \
-	/gxfs_work1/geomar/smomw235/data_user/data_mlandt/FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-3349_slp_monthly_1.nc \
+	/gxfs_work1/geomar/smomw235/data_user/data_mlandt/FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-3349_slp_monthly_1_midmonth.nc \
 	FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-2359_slp_monthly_1.nc
 ncks -O -d time,1,120,1 \
-	/gxfs_work1/geomar/smomw235/data_user/data_mlandt/FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-3349_tsw_monthly_1.nc \
+	/gxfs_work1/geomar/smomw235/data_user/data_mlandt/FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-3349_tsw_monthly_1_midmonth.nc \
 	FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-2359_tsw_monthly_1.nc
 ncks -O -d time,1,120,1 \
         /gxfs_work1/geomar/smomw235/data_user/data_mlandt/FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-3349_temp2_monthly_1.nc \
         FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-2359_temp2_monthly_1.nc
-ncks -O -d time_counter,1,60,1 -v sosaline \
+ncks -O -d time_counter,1,120,1 -v sosaline \
         /gxfs_work1/geomar/smomw235/data_user/data_mlandt/FOCI/FOCI1.3-SW038_1m_23500101_33491231_grid_T.nc \
-        FOCI/FOCI1.3-SW038_1m_23500101_23541231_grid_T.nc
+        FOCI/FOCI1.3-SW038_1m_23500101_23591231_grid_T.nc
 ncks -O -d time,1,120,1 \
-        /gxfs_work1/geomar/smomw235/data_user/data_mlandt/FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-3349_precip_monthly_1.nc \
+        /gxfs_work1/geomar/smomw235/data_user/data_mlandt/FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-3349_precip_monthly_1_midmonth.nc \
         FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-2359_precip_monthly_1.nc
