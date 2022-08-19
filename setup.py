894c98f828c2aae4552f942762f84a67ae33b4ad
Willi Rath <willirath@users.noreply.github.com>
2022-08-19 11:28:10 +0200

    Add docker image (#90)
    * Add 2stage Dockerfile
    
    * Add build workflow
    
    * Sudo ?
    
    * No sudo...
    
    * Fix setuptools-scm?
    
    * Manually set version
    
    * use build args?
    
    * Use calver tag
    
    * Se default ARG value
    
    * Use PEP440 compliant version number
    
    * Fix syntax...
    
    * Fix syntax...
    
    * Clean up
    
    * Set functioning default version
    
    * Replace DockerHub repo name.
    
    Co-authored-by: MarcoLandtHayen <95050365+MarcoLandtHayen@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
         "write_to": "climate_index_collection/_version.py",
         "write_to_template": '__version__ = "{version}"',
         "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
-    }
+        "local_scheme": "node-and-date",
+    },
 )
