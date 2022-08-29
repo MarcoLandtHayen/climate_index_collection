460e6a2b4c994ea7921c8d9c05262bebd1af999f
MarcoLandtHayen <95050365+MarcoLandtHayen@users.noreply.github.com>
2022-08-29 09:05:01 +0200

    Fix test data to start in January and modify script to extract test data accordingly (#109)

--- data/test_data/extract_test_data.sh
+++ data/test_data/extract_test_data.sh
@@ -8,43 +8,43 @@ mkdir -p FOCI CESM
 module load nco
 
 # extract 120 time steps of CESM data
-ncks -O -d time,1,120,1 \
-	/gxfs_work1/geomar/smomw235/data_user/data_mlandt/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0999.PSL.midmonth.nc \
+ncks -O -d time,0,119,1 \
+	/gxfs_work1/geomar/smomw511/climate_index_collection/data/raw/2022-08-22/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0999.PSL.midmonth.nc \
 	CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0010.PSL.nc
-ncks -O -d time,1,120,1 \
-	/gxfs_work1/geomar/smomw235/data_user/data_mlandt/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0999.SST.midmonth.nc \
+ncks -O -d time,0,119,1 \
+	/gxfs_work1/geomar/smomw511/climate_index_collection/data/raw/2022-08-22/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0999.SST.midmonth.nc \
 	CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0010.SST.nc
-ncks -O -d time,1,120,1 \
-	/gxfs_work1/geomar/smomw235/data_user/data_mlandt/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0999.Z500.midmonth.nc \
+ncks -O -d time,0,119,1 \
+	/gxfs_work1/geomar/smomw511/climate_index_collection/data/raw/2022-08-22/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0999.Z500.midmonth.nc \
 	CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0010.Z500.nc
-ncks -O -d time,1,120,1 \
-        /gxfs_work1/geomar/smomw235/data_user/data_mlandt/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0999.TS.nc \
+ncks -O -d time,0,119,1 \
+        /gxfs_work1/geomar/smomw511/climate_index_collection/data/raw/2022-08-22/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0999.TS.nc \
         CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0010.TS.nc
-ncks -O -d time,1,120,1 -v SALT \
-        /gxfs_work1/geomar/smomw235/data_user/data_mlandt/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.pop.h.0001-0999.SSS.nc \
-        CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.pop.h.0001-0010.SSS.nc
-ncks -O -d time,1,120,1 \
-        /gxfs_work1/geomar/smomw235/data_user/data_mlandt/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0999.PRECT.nc \
+ncks -O -d time,0,119,1 -v SALT \
+        /gxfs_work1/geomar/smomw511/climate_index_collection/data/raw/2022-08-22/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.pop.h.0001-0999.SSS.atmos_grid.nc \
+        CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.pop.h.0001-0010.SSS.atmos_grid.nc
+ncks -O -d time,0,119,1 \
+        /gxfs_work1/geomar/smomw511/climate_index_collection/data/raw/2022-08-22/CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0999.PRECT.nc \
         CESM/B1850WCN_f19g16_1000y_v3.2_mod-S15-G16.cam2.h0.0001-0010.PRECT.nc
 
 
 # extract 120 time steps of FOCI data
 
-ncks -O -d time,1,120,1 \
-	/gxfs_work1/geomar/smomw235/data_user/data_mlandt/FOCI/FOCI1.3-SW038_echam6_ATM_mm_2350-3349_geopoth_pl_monthly_50000_midmonth.nc \
+ncks -O -d time,0,119,1 \
+	/gxfs_work1/geomar/smomw511/climate_index_collection/data/raw/2022-08-22/FOCI/FOCI1.3-SW038_echam6_ATM_mm_2350-3349_geopoth_pl_monthly_50000_midmonth.nc \
 	FOCI/FOCI1.3-SW038_echam6_ATM_mm_2350-2359_geopoth_pl_monthly_50000.nc
-ncks -O -d time,1,120,1 \
-	/gxfs_work1/geomar/smomw235/data_user/data_mlandt/FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-3349_slp_monthly_1_midmonth.nc \
+ncks -O -d time,0,119,1 \
+	/gxfs_work1/geomar/smomw511/climate_index_collection/data/raw/2022-08-22//FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-3349_slp_monthly_1_midmonth.nc \
 	FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-2359_slp_monthly_1.nc
-ncks -O -d time,1,120,1 \
-	/gxfs_work1/geomar/smomw235/data_user/data_mlandt/FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-3349_tsw_monthly_1_midmonth.nc \
+ncks -O -d time,0,119,1 \
+	/gxfs_work1/geomar/smomw511/climate_index_collection/data/raw/2022-08-22/FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-3349_tsw_monthly_1_midmonth.nc \
 	FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-2359_tsw_monthly_1.nc
-ncks -O -d time,1,120,1 \
-        /gxfs_work1/geomar/smomw235/data_user/data_mlandt/FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-3349_temp2_monthly_1.nc \
+ncks -O -d time,0,119,1 \
+        /gxfs_work1/geomar/smomw511/climate_index_collection/data/raw/2022-08-22/FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-3349_temp2_monthly_1.nc \
         FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-2359_temp2_monthly_1.nc
-ncks -O -d time_counter,1,120,1 -v sosaline \
-        /gxfs_work1/geomar/smomw235/data_user/data_mlandt/FOCI/FOCI1.3-SW038_1m_23500101_33491231_grid_T.nc \
-        FOCI/FOCI1.3-SW038_1m_23500101_23591231_grid_T.nc
-ncks -O -d time,1,120,1 \
-        /gxfs_work1/geomar/smomw235/data_user/data_mlandt/FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-3349_precip_monthly_1_midmonth.nc \
+ncks -O -d time,0,119,1 -v sosaline \
+        /gxfs_work1/geomar/smomw511/climate_index_collection/data/raw/2022-08-22/FOCI/FOCI1.3-SW038_1m_23500101_33491231_grid_T_atmos_grid.nc \
+        FOCI/FOCI1.3-SW038_1m_23500101_23591231_grid_T_atmos_grid.nc
+ncks -O -d time,0,119,1 \
+        /gxfs_work1/geomar/smomw511/climate_index_collection/data/raw/2022-08-22/FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-3349_precip_monthly_1_midmonth.nc \
         FOCI/FOCI1.3-SW038_echam6_BOT_mm_2350-2359_precip_monthly_1.nc
